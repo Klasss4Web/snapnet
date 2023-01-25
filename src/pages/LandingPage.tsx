43ee9a348317ae8e5f20ba97733434e1411b66c9
Emmanuel Ochade <eochade16@gmail.com>
2023-01-25 15:19:17 +0100

    updated the direction

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,17 +3,18 @@ import bannerImg from "../assets/banner.png";
 export const LandingPage = () => {
   return (
     <div className="container mx-auto  p-4">
-      <div className="container flex justify-between">
-        <div className="flex-1">
-          <img width="100%" src={bannerImg} alt="banner" />
+      <div className="container flex justify-between  flex-col md:flex-row">
+        <div className="flex-1 h-90">
+          <img width="100%" height={"100%"} src={bannerImg} alt="banner" />
         </div>
         <div className="flex-1 flex items-center justify-end h-100 w-100">
-          <div className="text-end texts h-80">
+          <div className="md:text-end texts h-80">
             <h1
               className="text-xl large"
               style={{ width: "400px", fontWeight: "bold" }}
             >
-              Imagine if <span className="snap">Snapchat</span><br /> had events.
+              Imagine if <span className="snap">Snapchat</span>
+              <br /> had events.
             </h1>
             <p className="my-5" style={{ width: "400px" }}>
               Easily host and share events with your friends across any social
