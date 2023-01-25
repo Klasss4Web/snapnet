5543cfdeb0ae3b4125e971c3f4288050eb7af0ae
Emmanuel Ochade <eochade16@gmail.com>
2023-01-25 18:03:29 +0100

    added pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,45 @@
+export interface paginationData {
+  dataPerPage: number;
+  totalData: number;
+  currentPage: number;
+  paginate: (number: number) => void;
+}
+
+
+export const Pagination = ({
+  dataPerPage,
+  totalData,
+  paginate,
+  currentPage,
+}: paginationData) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalData / dataPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <nav>
+      <ul style={{ display: "flex" }}>
+        {pageNumbers?.map((number) => (
+          <li
+            key={number}
+            style={{
+              listStyle: "none",
+              marginRight: "20px",
+              border: "1px solid lightblue",
+              width: "40px",
+              textAlign: "center",
+              borderRadius: "5px",
+              cursor: "pointer",
+              background: currentPage === number ? "#2A9D8F" : "#fff",
+              color: currentPage === number ? "#fff" : "#2A9D8F",
+            }}
+            onClick={() => paginate(number)}
+          >
+            {number}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
--- src/pages/EventListPage.tsx
+++ src/pages/EventListPage.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { Events, getEvents } from "../service/api";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
+import { Pagination } from "../components/Pagination";
+
 
 const EventListPage = () => {
   const { data } = useQuery<Events[]>("events", getEvents, {
@@ -10,6 +12,16 @@ const EventListPage = () => {
   });
 
   const [SearchedData, setSearchedData] = useState<Events[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recordPerPage] = useState(5);
+
+    const indexOfLastRecord = currentPage * recordPerPage;
+    const indexOfFirstRecord = indexOfLastRecord - recordPerPage;
+    
+
+    //Change Page
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
 
   const handleFilter = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const results = data?.filter((dat) => {
@@ -35,12 +47,19 @@ const EventListPage = () => {
 
   // console.log("data", SearchedData);
   const mapData = SearchedData.length > 0 ? SearchedData : data;
+
+  const currentRecord = mapData?.slice(indexOfFirstRecord, indexOfLastRecord);
   return (
     <div className="App container mx-auto  p-4">
       <h1>List of events in our archive</h1>
-      <input placeholder="Search event by title, organizer or location" onChange={handleFilter} />
-      <button className="mx-2" onClick={handleFilterByPetsAllowed}>Pets Allowed?</button>
-      {mapData?.map((datum) => {
+      <input
+        placeholder="Search event by title, organizer or location"
+        onChange={handleFilter}
+      />
+      <button className="mx-2" onClick={handleFilterByPetsAllowed}>
+        Pets Allowed?
+      </button>
+      {currentRecord?.map((datum) => {
         return (
           <Link
             className="article-list-linkstyle"
@@ -59,8 +78,18 @@ const EventListPage = () => {
         );
       })}
       {/* <EventList otherEvent={data} /> */}
+      <div className="flex justify-end">
+        <Pagination
+          dataPerPage={recordPerPage}
+          totalData={data?.length ?? 0}
+          currentPage={currentPage}
+          paginate={paginate}
+        />
+      </div>
     </div>
   );
 };
+
+
 
 export default EventListPage;
